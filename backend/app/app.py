e89ecc9e0d2bab0ed21650bea63e4ec3e83189d3
Yaroslav Nestor <nestorslavko45@gmail.com>
2020-12-06 14:35:09 +0200

    Add middleware for FastAPI application. Change query params

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routes import router as events_router
+
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
 
 
 def create_app():
     fast_api_application = FastAPI(title="Google Clone API")
 
     fast_api_application.include_router(events_router)
+
+    fast_api_application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"])
 
     return fast_api_application
 
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,12 +8,12 @@ from .services.indexing_service import WebPageIndexing
 router = APIRouter()
 
 
-@router.get("/index/", response_model=IndexingResultModel)
-async def index_page(url: str, max_recursion: int = 3):
-    indexing_result = await WebPageIndexing(url, max_recursion).index_web_site()
+@router.get("/index", response_model=IndexingResultModel)
+async def index_page(q: str, max_recursion: int = 3):
+    indexing_result = await WebPageIndexing(q, max_recursion).index_web_site()
     return indexing_result
 
 
-@router.get("/search/", response_model=List[SearchResultsModel])
+@router.get("/search", response_model=List[SearchResultsModel])
 async def search_pages(search: str):
     return {"search": search}
