25a1dae388c4bef5c7d928d39ef549ac33d99c98
Yaroslav Nestor <nestorslavko45@gmail.com>
2020-12-07 02:42:47 +0200

    Add Dockerfile for backend application

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         app="app.app:app",
-        host="localhost",
+        host="0.0.0.0",
         port=8000,
         reload=True
     )
