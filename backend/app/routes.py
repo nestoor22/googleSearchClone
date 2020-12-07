1edf2f20d7eb35edfb89540c40a4559fcba4b42e
Yaroslav Nestor <nestorslavko45@gmail.com>
2020-12-07 02:08:04 +0200

    Add offset and limit for search results

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -12,11 +12,12 @@ router = APIRouter()
 @router.get("/index", response_model=IndexingResultModel)
 async def index_page(q: str, max_recursion: int = 3):
     indexing_result = await WebPageIndexing(q, max_recursion).index_web_site()
+
     return indexing_result
 
 
 @router.get("/search", response_model=List[SearchResultsModel])
-async def search_pages(q: str):
-    pages_info = await SearchService(q).get_search_results()
+async def search_pages(q: str, offset: int = 0, limit: int = 10):
+    pages_info = await SearchService(q).get_search_results(offset, limit)
 
     return pages_info
